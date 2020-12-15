1b901e3d0ed29c6a1a78227ff1f46874ccd14ac7
mateovalenciaeo <56328747+mateovalenciaeo@users.noreply.github.com>
2020-12-14 23:48:41 -0500

    MongoDB sample tested with Mocha

---
+++ test/reading_test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', () => {
+    let joe;
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        joe.save().then(() => {
+            assert(!joe.isNew);
+            done();
+        });
+    });
+
+    it('finds all users with the name of joe', (done) => {
+        User.find({ name: 'Joe' }).then((users) => {
+            assert(users[0].id === joe.id);
+            done();
+        });
+    });
+
+    it('finds a user with a particular id', (done) => {
+        User.findOne({ _id: joe._id }).then((user) => {
+            assert(user.name === 'Joe');
+            done();
+        });
+    });
+});
